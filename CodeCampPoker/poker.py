88aa36ebe74919020ed2b38b11e3c1354580dd81
ashishk144 <ashishk144>
2018-08-14 11:35:21 +0530

    " -> 0 of 3 passed. pylint: 9.29/10 "

--- CodeCampPoker/poker.py
+++ CodeCampPoker/poker.py
@@ -31,7 +31,7 @@ def is_straight(hand):
         Write the code for it and return True if it is a straight else return False
     '''
     temp_hand = sorted(hand)
-    #print(temp_hand)
+    print(temp_hand)
     for i in range(len(temp_hand)-1):
         if (card_value(temp_hand[i+1]) - card_value(temp_hand[i])) != 1:
             return False
