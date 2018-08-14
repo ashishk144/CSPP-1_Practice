beaa845af9109af210762180281eda15f7ec3383
ashishk144 <ashishk144>
2018-08-14 12:22:18 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CodeCampPoker/poker.py
+++ CodeCampPoker/poker.py
@@ -7,18 +7,19 @@
 # H_CARDS = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH']
 # S_CARDS = ['2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS']
 # C_CARDS = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC']
-def card_value(x):
-    if x[0] == 'T':
+def card_value(x_x):
+    '''integer casting function'''
+    if x_x[0] == 'T':
         return 10
-    if x[0] == 'J':
+    if x_x[0] == 'J':
         return 11
-    if x[0] == 'Q':
+    if x_x[0] == 'Q':
         return 12
-    if x[0] == 'K':
+    if x_x[0] == 'K':
         return 13
-    if x[0] == 'A':
+    if x_x[0] == 'A':
         return 14
-    return int(x[0])
+    return int(x_x[0])
 
 def is_straight(hand):
     '''
